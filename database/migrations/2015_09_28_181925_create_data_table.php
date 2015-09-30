bf84db640ac00c2fca4cd013b6a4c53535fd3b34
Jimmy Kra <KRA16589@spengergasse>
2015-09-30 16:56:31 +0200

    further test

--- database/migrations/2015_09_28_181925_create_data_table.php
+++ database/migrations/2015_09_28_181925_create_data_table.php
@@ -15,6 +15,7 @@ class CreateDataTable extends Migration
         Schema::create('data', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps('created_at');
+            //comment
         });
     }
 
