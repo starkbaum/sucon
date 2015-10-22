51f9929cd8dafab04d302c65da701eaa48fc1504
starkbaum <starkbaum.stefan@gmail.com>
2015-10-22 16:05:44 +0200

    renamed class

--- database/migrations/2015_10_01_074814_create_data_has_ratings_Table.php
+++ database/migrations/2015_10_01_074814_create_data_has_ratings_Table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class DataHasRatingsTable extends Migration
+class CreateDataHasRatingsTable extends Migration
 {
     /**
      * Run the migrations.
