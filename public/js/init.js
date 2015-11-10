432e9dff0c55daaf72238238c9cfeeb901496f35
starkbaum <starkbaum.stefan@gmail.com>
2015-11-10 18:30:36 +0100

    design improves upload modal

--- public/js/init.js
+++ public/js/init.js
@@ -4,4 +4,5 @@ $(document).ready(function(){
     $('.parallax').parallax();
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
+    $('select').material_select();
 });
--- resources/views/partials/modals/addFileModal.blade.php
+++ resources/views/partials/modals/addFileModal.blade.php
@@ -1,3 +1,5 @@
+<form action="{{URL::to('courses/upload')}}" method="post" enctype="multipart/form-data">
+
 <!-- Modal Structure -->
 <div id="modal1" class="modal bottom-sheet">
     <div class="container">
@@ -5,26 +7,36 @@
         <div class="modal-content">
             <h4>Datei hinzufügen</h4>
             <p>Nachdem die Datei hinzugefügt wird, muss noch die Freigabe eines Administrators erfolgen bevor sie ersichtlich wird</p>
-            <p>{{ $course->slug }}</p>
 
 
-            <form action="{{URL::to('courses/upload')}}" method="post" enctype="multipart/form-data">
 
-                <br>
-                <label for="filename">Filename</label>
-                <input type="text" name="filename" id="filename"/>
-                <br>
-                <label for="file">Select Image to upload please</label>
-                <input type="file" name="file" id="file" />
+                <!-- Filename -->
+                <div class="row">
+                    <div class="input-field col s6">
+                        <input id="filename" name="filename" type="text" class="validate">
+                        <label for="filename">Dateiname</label>
+                    </div>
+                </div>
+                <!-- File Selection -->
+                <div class="row">
+                    <div class="file-field input-field col s6">
+                        <div class="btn">
+                            <span>Datei</span>
+                            <input type="file" name="file" id="file">
+                        </div>
+                        <div class="file-path-wrapper">
+                            <input class="file-path validate" type="text">
+                        </div>
+                    </div>
+                </div>
+
                 <input type="hidden" name="test" value="{{ $course->path_to_material }}"/>
                 <input type="hidden" name="courseId" value="{{ $course->id }}"/>
-                <input type="submit" name="submit" value="submit"/>
                 <input type="hidden" value = "{{csrf_token()}}" name="_token"/>
-            </form>
-
     </div>
     <div class="modal-footer">
-        <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Freigabe anfordern</a>
+        <button class="waves-effect waves-teal btn-flat" type="submit" name="action">Freigabe anfordern</button>
     </div>
 </div>
 </div>
+</form>
