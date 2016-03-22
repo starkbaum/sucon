f7f179a7826dc07300896310a02c3f32902ba09f
starkbaum <STA16638@spengergasse.at>
2016-03-22 11:19:18 +0100

    added Test for User login and registry

---
+++ tests/integration/UserManagementTest.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Faker\Factory as Faker;
+
+class UserManagementTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * @var FakerGenerator
+     */
+    protected $faker;
+
+    /**
+     * Setup faker
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->faker = Faker::create();
+    }
+
+    /** @test */
+    public function a_user_can_login()
+    {
+        $this->visit('/auth/login');
+        $this->type('jane@cbi.com', 'email');
+        $this->type('patrickJane', 'password');
+        $this->press('action');
+        $this->seePageIs('/courses');
+    }
+
+    /** @test */
+    public function a_user_can_register()
+    {
+        $userName = $this->faker->name;
+        $userEmail = $this->faker->email;
+        $userPassword = 'password';
+
+        $this->visit('/auth/register');
+        $this->type($userName, 'name');
+        $this->type($userEmail, 'email');
+        $this->type($userPassword, 'password');
+        $this->type($userPassword, 'password_confirmation');
+        $this->press('action');
+        $this->seePageIs('/courses');
+        $this->seeInDatabase('users', ['name' => $userName, 'email' => $userEmail]);
+    }
+
+}
