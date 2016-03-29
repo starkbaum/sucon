25552af5606c3828d7057ca93ecc87663c61f846
starkbaum <starkbaum.stefan@gmail.com>
2016-03-29 20:11:30 +0200

    bugfix issue #15 changed from fab to normal button

--- resources/views/partials/searchForm.blade.php
+++ resources/views/partials/searchForm.blade.php
@@ -4,9 +4,7 @@
             {!! Form::text('search-term', null, ['class' => 'form-control', 'placeholder' => 'Suche']) !!}
         </li>
         <li>
-            <button class="btn-floating waves-effect waves-light sucon-background-orange" type="submit">
-                <i class="tiny material-icons right">search</i>
-            </button>
+            <button class="btn sucon-background-orange" type="submit">Suche</button>
         </li>
     </ul>
 </form>
